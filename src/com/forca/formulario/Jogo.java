75f8091bbf31f1a7ef0f67a1c1c44df2038bb04a
danielgregorio@gmail.com <danielgregorio@gmail.com@e4cb5200-a474-11dd-a8c6-677c70d10fe4>
2008-11-22 00:49:30 +0000

    imagem !!!

--- src/com/forca/formulario/Jogo.java
+++ src/com/forca/formulario/Jogo.java
@@ -2,9 +2,12 @@ package com.forca.formulario;
 
 import com.forca.cliente.Cliente;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,23 +21,35 @@ public class Jogo extends JFrame implements ActionListener{
 	private JTextField caixa;
 	private JButton botao;
 	private JLabel dica;
+	public Toolkit tk = Toolkit.getDefaultToolkit();
+	public JLabel imagem;
+	public JFrame frame; 
+	//Image img0 = tk.getImage(getClass().getResource("/icones/handset.jpg"));  
 	
 	public Jogo(){
-		this.setSize(250,125);
-		this.setLocation(400, 300);
+		this.setSize(250,140);
+		this.setLocation(0, 375);
 		this.setTitle("Jogo da Forca");
-		this.setLayout(new GridLayout(3,1));
+		this.setLayout(new GridLayout(2,1));
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		label = new JLabel("Jogo da Forca");
 		dica = new JLabel();
 		caixa = new JTextField();
 		botao = new JButton("Enviar");
 		botao.addActionListener(this);
+		imagem = new JLabel();
 		this.add(label);
 		this.add(dica);
 		this.add(caixa);
 		this.add(botao);
 		
+		
+		frame = new JFrame();
+		frame.setLayout(new GridLayout(1,1));
+		frame.setSize(250,375);
+		frame.setLocation(0,0);
+		frame.add(imagem);
+		frame.setVisible(true);
 	}
 
 	public void actionPerformed(ActionEvent arg0) {
@@ -73,6 +88,21 @@ public class Jogo extends JFrame implements ActionListener{
 
 	public void setDica(JLabel dica) {
 		this.dica = dica;
+	}
+
+	public void setImagem(int erros) {
+		 System.out.println("erros -> "+erros);
+		 
+		 Image img = tk.getImage(getClass().getResource(Integer.toString(erros)+".jpg"));      
+		 ImageIcon icone = new ImageIcon(img);
+		 //remove(imagem);
+		 imagem.setIcon(icone);
+		 //frame.add(imagem);
+		 //frame.setVisible(true);
+		 //this.add(imagem);
+		 frame.repaint();
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
